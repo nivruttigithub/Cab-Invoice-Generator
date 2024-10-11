1a12771c6635e95cbe85f9f0e7c3cf377724c131
NivruttiWagh <nivruttiwagh2002@gmail.com>
2024-10-11 11:03:33 +0530

    Added : Fare Calculator

---
+++ src/test/java/com/bridgelabz/invoicegenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.invoicegenerator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceGeneratorTest {
+
+    @Test
+    public void testCalculateFare() {
+        //Arrange
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        //Act
+        double distance1 = 3;
+        double time1 = 15;
+        double expectedFare = (3 * 10) + (15 * 1);
+        //Assert
+        Assertions.assertEquals(expectedFare, cabInvoiceGenerator.calculateFare(distance1, time1));
+    }
+
+}
